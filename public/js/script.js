3586022ad18de14ac21cda23d0b4b54c2c94a205
Prashanth R <rprashanth1211@gmail.com>
2023-03-27 14:17:43 +1100

    SIT725 - Image changes and text changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,37 +1,43 @@
 const cardList = [
     {
-        title: "Kitten 2",
-        image: "images/kitten-2.jpg",
-        link: "About Kitten 2",
-        desciption: "Demo desciption about kitten 2"
+        title: 'St.Kilda Beach', image: 'image/st.Kilda.png', link: 'About St.Kilda', description: 'St.Kilda description'
     },
     {
-
-        title: "Kitten 3",
-        image: "images/kitten-3.jpg",
-        link: "About Kitten 3",
-        desciption: "Demo desciption about kitten 3"
+        title: 'Altona Beach', image: 'image/Altona Beach.png', link: 'About Altona Beach', description: 'Altona Beach description'
     }
-]
+];
+
 const clickMe = () => {
-    alert("Thanks for clicking me. Hope you have a nice day!")
+    console.log('clickMe clicked');
 }
+
 const addCards = (items) => {
+    console.log(items);
     items.forEach(item => {
-        let itemToAppend = '<div class="col s4 center-align">'+
-    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
-    '</div><div class="card-content">'+
-    '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
-    '<div class="card-reveal">'+'<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+       '<p class="card-text">'+item.desciption+'</p>'+
-      '</div></div></div>';
-      $("#card-section").append(itemToAppend)
+        let itemToAppend = '<div class="col s4 center-align"> <div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+ item.image +'"></img></div><div class="card-content"><span class="card-title activator grey-text text-darken-4">'+ item.title +'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div><div class="card-reveal"><span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span><p class="card-text">'+item.description+'</p></div></div></div>';
+        $('#card-section').append(itemToAppend);
     });
+}
+
+const submitForm = () => {
+    let formData = {};
+    formData.first_name = $('#first_name').val();
+    formData.last_name = $('#last_name').val();
+    formData.email = $('#email').val();
+    formData.password = $('#password').val();
+    document.getElementById('nametag').innerHTML=formData.first_name;
+
+    console.log('form data: ', formData);
 }
 
 $(document).ready(function(){
     $('.materialboxed').materialbox();
-    $('#clickMeButton').click(()=>{
-        clickMe();
-    })
+    $('.modal').modal();
+
     addCards(cardList);
-  });
+    $('#formSubmit').click(()=>{
+        submitForm();
+        
+    })
+    
+});
